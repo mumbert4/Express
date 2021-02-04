9b03895d1b29b0e8e71e30db91158d6419324428
Miquel <miquel.umbert.bosch@gmail.com>
2021-02-04 10:02:37 +0100

    Nodemon afegit

--- index.js
+++ index.js
@@ -26,7 +26,22 @@ app.get('/',(req,res)=>{
 //sense aixo mos sortira error perque sa pagina mos demana una peticio i noltros no li tornam cap resposta 
 
 
-app.listen(3000,()=>{
-    console.log('Servidor en port 3000');
+app.get('/about',(req,res)=>{
+    res.send('Som en Miquel Umbert')
+})
+
+app.get('/contact',(req,res)=>{
+    res.send('From contact')
 });
+
+app.get('/test',(req,res)=>{
+    res.send('Test fet');
+})
+
+
+app.listen(5000,()=>{
+    console.log('Servidor en port 5000');
+});
+
+
 
